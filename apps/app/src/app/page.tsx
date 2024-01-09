58f91375dac901ebe59cae19c241553520fb149c
NBK-01 <nayefkanaan03@gmail.com>
2024-01-09 15:53:42 +0200

    Init

---
+++ apps/app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Card } from '@repo/ui/card'
+
+export default function Page(): JSX.Element {
+  return (
+    <Card href="/" title="Test"> Hello </Card>
+  )
+}
+
---
+++ apps/www/src/app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Page(): JSX.Element {
+  return (
+    <h1> Landing </h1>
+  )
+}
+
